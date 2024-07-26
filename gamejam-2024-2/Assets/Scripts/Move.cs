4f06da628ee6dcc6ff45951942cdf1ce5c13fe44
Juan Vitor <juanriven10@gmail.com>
2024-07-26 20:13:48 -0300

    🚶 Movimentação Simples

---
+++ gamejam-2024-2/Assets/Scripts/Move.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+    public CharacterController controller;
+    public float speed = 12f;
+
+    public Transform modelo;
+
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (move != Vector3.zero) {
+            modelo.forward = move.normalized;
+        }
+    }
+}
